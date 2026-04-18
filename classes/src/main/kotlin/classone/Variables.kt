7bd1b30106b55574300b9aba54fd9f6a445e57fa
Igor Cunha <gigor497@gmail.com>
2026-04-17 22:53:28 -0300

    classone variables

---
+++ classes/src/main/kotlin/classone/Variables.kt
@@ -0,0 +1,13 @@
+package classone
+
+fun main() {
+    val nameA : String = "John"
+    println(nameA)
+
+    var nameB : String = "Paul"
+    println(nameB)
+
+    nameB = "Peter"
+    println(nameB)
+
+}
